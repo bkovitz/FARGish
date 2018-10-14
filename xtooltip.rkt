f29c6ec80e4933636781c50f910a4b5567c90638
Ben Kovitz <bkovitz@indiana.edu>
2018-10-14 08:46:37 -0400

    Tooltipping labeled dots sort of works

--- xtooltip.rkt
+++ xtooltip.rkt
@@ -2,10 +2,21 @@
 
 #lang debug at-exp racket/gui
 
-(require (prefix-in pict: pict))
+(require (prefix-in pict: pict) pict/snip mrlib/snip-canvas)
 ;(require drracket/private/tooltip)
 
 (define fr (new frame% [label "xtooltip"] [width 200] [height 100]))
+
+;CONFUSION: This is needed only because pane% doesn't support client->screen.
+;Is the reason why it doesn't also a reason why this function shouldn't exist?
+(define (window-parent-of window)
+  (let ([parent (send window get-parent)])
+    (cond
+      [(not parent)
+       #f]
+      [(is-a? parent window<%>)
+       parent]
+      [else (window-parent-of parent)])))
 
 ;CONFUSION: Is the documentation on client->screen or get-current-mouse-state
 ;wrong?
@@ -13,7 +24,7 @@
   (let-values ([(xo yo) (get-display-left-top-inset)])
     (values (- xo) (- yo))))
 (define (window-top-left-in-screen-coordinates window)
-  (let ([parent (send window get-parent)])
+  (let ([parent (window-parent-of window)])
     (if parent
       (let-values ([(wx wy) (send parent client->screen (send window get-x)
                                                         (send window get-y))])
@@ -32,22 +43,26 @@
          [text-image (for/fold ([text-image (pict:blank)])
                                ([line text])
                        (pict:vl-append text-image (pict:text line)))]
-         [text-image (pict:inset text-image 3 2)])
-    (pict:cc-superimpose (pict:filled-rectangle (pict:pict-width text-image)
-                                                (pict:pict-height text-image)
-                                                #:color "LemonChiffon")
-                         text-image)))
+         [text-image (pict:inset text-image 4 2)]
+         [background (pict:filled-rectangle
+                       (ceiling (pict:pict-width text-image))
+                       (ceiling (pict:pict-height text-image))
+                       #:color "LemonChiffon"
+                       #:draw-border? #t)])
+    (pict:cc-superimpose background text-image)))
 
 (define pict-canvas%  ; <--- CODE SMELL: reinventing the wheel (pict.rkt)
   (class canvas%
-    (init-field pict)
+    (init-field pict
+                [style '()])
     (inherit get-dc)
     (define/override (on-paint)
       (pict:draw-pict pict (get-dc) 0 0))
     (super-new [min-width (exact-ceiling (pict:pict-width pict))]
                [min-height (exact-ceiling (pict:pict-height pict))]
                [stretchable-width #f]
-               [stretchable-height #f])))
+               [stretchable-height #f]
+               [style (cons 'transparent style)])))
 
 (define tooltip-window%
   (class frame%
@@ -60,14 +75,14 @@
                [label ""]
                [width width]
                [height height]
-               [x (exact-ceiling (- (send point get-x) (/ width 2) 3))]
-               [y (exact-ceiling (- (send point get-y) height 8))]
                [stretchable-width #f]
-               [stretchable-height #f])
+               [stretchable-height #f]
+               [x (exact-ceiling (- (send point get-x) (/ width 2) 3))]
+               [y (exact-ceiling (- (send point get-y) height 8))])
     (define canvas (new pict-canvas% [pict pict] [parent this]))
     (send this show #t)))
 
-(define TOOLTIP-HOVER-DELAY 750)
+(define TOOLTIP-HOVER-DELAY 600)
   ;When mouse cursor sits motionless over relevant window for this long,
   ;tooltip appears.
 
@@ -79,7 +94,6 @@
 
     (define (maybe-open-tooltip-window)
       (define-values (point buttons) (get-current-mouse-state))
-      (list (send point get-x) (send point get-y))
       (when (and (null? buttons) (in-window? this point))
         (set! tooltip-window (new tooltip-window% [text tooltip]
                                                   [point point]))))
@@ -104,13 +118,60 @@
             (send timer start TOOLTIP-HOVER-DELAY #t))
           #t)  ; UNSURE: What is on-subwindow-event supposed to return here?
         #f))))
-      ;BUG: Often no motion event comes when the mouse leaves this window,
+      ;BUG: Often no 'motion event comes when the mouse leaves this window,
       ;so the tooltip stays up.
 
 (define m (new (tooltip-mixin message%)
                [parent fr] [label "Label"] [tooltip 22.6]))
 (define b (new (tooltip-mixin button%)
                [parent fr] [label "Button"] [tooltip "Press this"]))
+
+(define hp (new horizontal-pane% [parent fr] [alignment '(left top)]))
+
+(define tpc% (tooltip-mixin pict-canvas%))
+
+(define (disk d)
+  (pict:cc-superimpose
+    (pict:ghost (pict:disk 50))
+    (pict:disk d #:color "aquamarine" #:draw-border? #f)))
+
+(define (make-dot parent label activation)
+  (define vp (new vertical-pane% [parent parent]
+                                 [stretchable-width #f]
+                                 [stretchable-height #f]))
+  (define l (new message% [parent vp] [label label]))
+  (define d (new tpc% [parent vp]
+                      [pict (disk (* 8.0 activation))]
+                      [tooltip activation]))
+  vp)
+
+;(define tpc1 (new tpc% [parent hp] [pict (disk 30)] [tooltip 22.6]))
+;(define tpc2 (new tpc% [parent hp] [pict (disk 3)] [tooltip 2.6]))
+(define d1 (make-dot hp "archetype4" 4.1))
+(define d2 (make-dot hp "some-sa-node" 2.26))
+(define d3 (make-dot hp "this-dot" 0.4))
+
+(define inert-pasteboard%  ; <--- CODE SMELL: reinventing wheel?
+  (class pasteboard%
+    (define/augment (can-select? _)
+      #f)
+    (super-new)))
+
+(define ROW-HEIGHT 100)
+(define LABEL-OFFSET 5)
+(define DOT-CENTER-OFFSET 65)
+(define DIAMETER-RATIO 8.0)
+
+;(define sa-canvas%
+;  (class editor-canvas%
+;    (init-field [max-width 1000]
+;                [pb (new inert-pasteboard%)])
+;    (define current-row 0)
+;    (define current-x 0)
+;    (define/public (insert-dot label activation-level)
+;      (send pb insert (new pict-snip%
+;    (super-new [editor pb])))
+
 (send fr show #t)
 
 ;(define tf (new tooltip-frame% [frame-to-track m]))
@@ -124,3 +185,16 @@
 ;  (send tf show-over x y w h))
 ;#;(send tf show #t)
 
+; QUESTIONS
+;
+;  How do you close a frame in racket/gui?
+;
+;  What are "screen coordinates"? Are they different for client->screen
+;  and get-current-mouse-state?
+;
+;  Isn't snip-canvas% there so you can have an active "snip" GUI object
+;  without an editor? But doesn't that undermine the point of snips--objects
+;  within an editor?
+;
+;  Probable bug: overlapping windows could have both tooltips showing
+;  simultaneously.
