9ca4a655bca729782360bc097d7623739dd109a5
Ben Kovitz <bkovitz@indiana.edu>
2019-03-05 06:11:51 -0500

    Started a Typed Racket version of fargish.rkt...

--- fargish.rkt
+++ fargish.rkt
@@ -8,6 +8,52 @@
 
 ; Generate make-node in the main macro?
 
-(struct raw-nodeclass* ([name : Symbol]
-                        [args : (Listof Symbol)]
-                        [display-name : (-> 
+(struct Nodeclass ([name : Symbol]
+                   [args : (Listof Symbol)]
+                   [parents : (Listof Symbol)])
+                  #:transparent)
+
+;(define-syntax (farg-model-spec stx)
+(define-syntax (spec stx)
+  (define-syntax-class name+args
+    #:description "name or name with arguments"
+    #:attributes [name (arg 1)]
+    (pattern name:id
+             #:with (arg ...) #'())
+    (pattern (name:id arg:id ...)))
+
+  (define-syntax-class nodeclass
+    #:description "nodeclass"
+    #:datum-literals [nodeclass]
+    #:attributes [name (arg 1) (parent 1) value]
+    (pattern (nodeclass ~! decl:name+args body:nodeclass-body)
+             #:with name #'decl.name
+             #:with (arg ...) #'(decl.arg ...)
+             #:with (parent ...) #'(body.parent ... ...)
+             #:with value #'(~? body.value-expr #f)
+    ))
+
+  (define-splicing-syntax-class nodeclass-body
+    #:description "nodeclass body"
+    #:datum-literals [is-a archetype value links-into applies-to]
+    #:attributes [(parent 2) value-expr]
+    (pattern (~seq
+      (~alt (is-a ~! parent:expr ...+)
+            (~optional (value ~! value-expr:expr)
+                       #:too-many "'value' specified more than once")
+            ) ...)
+       #:do [(println #'(~? value-expr #f))]))
+
+  (syntax-parse stx
+    [(_ nc:nodeclass ...)
+     #:with (nc-struct ...)
+            #'((Nodeclass 'nc.name '(nc.arg ...) (list nc.parent ...)) ...)
+     #`(list nc-struct ...)]))
+
+(spec (nodeclass (blah a b c)))
+
+(spec (nodeclass blah (is-a 'x 'y) (is-a 'z)))
+
+(spec
+  (nodeclass blah
+    (value 22)))
