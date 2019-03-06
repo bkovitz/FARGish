14bbc03be294752cbfa3f830cdc4f0a3c06ee9f4
Ben Kovitz <bkovitz@indiana.edu>
2019-03-06 16:57:05 -0500

    Chipping away at fargish.rkt ...
    Now when you define a nodeclass like (number n), the macro defines
    a function called 'number that returns a hash table of all the
    attrs for a 'number node except id. The idea is that make-node can
    take this hash table as an argument instead of the present system
    of passing the arguments through make-node.

--- fargish.rkt
+++ fargish.rkt
@@ -3,32 +3,42 @@
 #lang typed/racket
 
 (require syntax/parse syntax/parse/define
-         (for-syntax syntax/parse))
-(require "wheel.rkt")
+         (for-syntax racket/syntax syntax/parse))
+(require "types.rkt" "typed-wheel.rkt")
 
 ; Generate make-node in the main macro?
 
 (struct Nodeclass ([name : Symbol]
-                   [args : (Listof Symbol)]
+                   ;[args : (Listof Symbol)]
                    [parents : (Listof Symbol)])
                   #:transparent)
 
+(struct FARGishSpec ([class->parents : (Hashof Symbol (Setof Symbol))])
+                    #:transparent)
+
 ;(define-syntax (farg-model-spec stx)
-(define-syntax (spec stx)
+(define-syntax (define-spec stx)
+  (define-syntax-class arg+type
+    #:description "argument"
+    #:datum-literals [:]
+    (pattern [argname:id : type:expr]))
+
   (define-syntax-class name+args
-    #:description "name or name with arguments"
-    #:attributes [name (arg 1)]
+    #:description "name maybe with arguments"
+    #:attributes [name (arg 1) (arg.type 1)]
     (pattern name:id
-             #:with (arg ...) #'())
-    (pattern (name:id arg:id ...)))
+             #:with (arg ...) #'()
+             #:with (arg.type ...) #'())
+    (pattern (name:id arg:arg+type ...)))
 
   (define-syntax-class nodeclass
     #:description "nodeclass"
     #:datum-literals [nodeclass]
-    #:attributes [name (arg 1) (parent 1) value]
+    #:attributes [name (arg 1) (arg.type 1) (parent 1) value]
     (pattern (nodeclass ~! decl:name+args body:nodeclass-body)
              #:with name #'decl.name
              #:with (arg ...) #'(decl.arg ...)
+             #:with (arg.type ...) #'(decl.arg.type ...)
              #:with (parent ...) #'(body.parent ... ...)
              #:with value #'(~? body.value-expr #f)
     ))
@@ -40,20 +50,39 @@
     (pattern (~seq
       (~alt (is-a ~! parent:expr ...+)
             (~optional (value ~! value-expr:expr)
-                       #:too-many "'value' specified more than once")
+                       #:too-many "'value' specified more than once"
+                       #:defaults
+                         ([value (syntax-property #'(void) 'missing? #t)]))
             ) ...)
-       #:do [(println #'(~? value-expr #f))]))
+       #:do [(println #'(~? (parent ... ...) #f))]))
 
   (syntax-parse stx
-    [(_ nc:nodeclass ...)
-     #:with (nc-struct ...)
-            #'((Nodeclass 'nc.name '(nc.arg ...) (list nc.parent ...)) ...)
-     #`(list nc-struct ...)]))
+    [(_ spec-name:id nc:nodeclass ...)
+;     #:with (nc-struct ...)
+;            #'((Nodeclass 'nc.name '(nc.arg ...) (list nc.parent ...)) ...)
+;     #`(list nc-struct ...)
+     ;#:with ht/class->is-a (format-id stx "ht/class->is-a" #:source stx)
+     #`(begin
+         (begin (: nc.name : nc.arg.type ... -> (Hashof Symbol Any))
+                (define (nc.name nc.arg ...) (hash 'value nc.value
+                                                   'class 'nc.name))) ...
+         (: spec-name : FARGishSpec)
+         (define spec-name
+           (FARGishSpec (hash (~@ 'nc.name (set 'nc.name 'nc.parent ...)) ...)))
+         )]))
 
-(spec (nodeclass (blah a b c)))
+;(spec (nodeclass (blah a b c)))
+;
+;(spec (nodeclass blah (is-a 'x 'y) (is-a 'z)))
+;
+;(spec
+;  (nodeclass blah
+;    (value 22)))
 
-(spec (nodeclass blah (is-a 'x 'y) (is-a 'z)))
+(define-spec spec
+  (nodeclass (number [n : Integer])
+    (value n))
+  (nodeclass plus))
+(number 17)
+;hey
 
-(spec
-  (nodeclass blah
-    (value 22)))
