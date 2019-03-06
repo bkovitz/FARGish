4a4ab0d556784743ac9f5059a63283fa77bbc618
Ben Kovitz <bkovitz@indiana.edu>
2019-03-06 17:16:34 -0500

    Added display-name ...

--- fargish.rkt
+++ fargish.rkt
@@ -34,25 +34,33 @@
   (define-syntax-class nodeclass
     #:description "nodeclass"
     #:datum-literals [nodeclass]
-    #:attributes [name (arg 1) (arg.type 1) (parent 1) value]
+    #:attributes [name (arg 1) (arg.type 1) (parent 1) value display-name]
     (pattern (nodeclass ~! decl:name+args body:nodeclass-body)
              #:with name #'decl.name
              #:with (arg ...) #'(decl.arg ...)
              #:with (arg.type ...) #'(decl.arg.type ...)
              #:with (parent ...) #'(body.parent ... ...)
              #:with value #'(~? body.value-expr #f)
+             #:with display-name
+                      (if (syntax-property #'body.display-name-expr 'missing?)
+                        #''name
+                        #'body.display-name-expr)
     ))
 
   (define-splicing-syntax-class nodeclass-body
     #:description "nodeclass body"
     #:datum-literals [is-a archetype value links-into applies-to]
-    #:attributes [(parent 2) value-expr]
+    #:attributes [(parent 2) value-expr display-name-expr]
     (pattern (~seq
       (~alt (is-a ~! parent:expr ...+)
             (~optional (value ~! value-expr:expr)
                        #:too-many "'value' specified more than once"
-                       #:defaults
-                         ([value (syntax-property #'(void) 'missing? #t)]))
+                       #:defaults ([value
+                                     (syntax-property #'(void) 'missing? #t)]))
+            (~optional (display-name ~! display-name-expr:expr)
+                       #:too-many "'display-name' specified more than once"
+                       #:defaults ([display-name-expr
+                                     (syntax-property #'(void) 'missing? #t)]))
             ) ...)
        #:do [(println #'(~? (parent ... ...) #f))]))
 
@@ -64,8 +72,11 @@
      ;#:with ht/class->is-a (format-id stx "ht/class->is-a" #:source stx)
      #`(begin
          (begin (: nc.name : nc.arg.type ... -> (Hashof Symbol Any))
-                (define (nc.name nc.arg ...) (hash 'value nc.value
-                                                   'class 'nc.name))) ...
+                (define (nc.name nc.arg ...)
+                  (let ([dispname : DisplayName nc.display-name])
+                    (hash 'value nc.value
+                          'display-name dispname
+                          'class 'nc.name)))) ...
          (: spec-name : FARGishSpec)
          (define spec-name
            (FARGishSpec (hash (~@ 'nc.name (set 'nc.name 'nc.parent ...)) ...)))
@@ -81,6 +92,7 @@
 
 (define-spec spec
   (nodeclass (number [n : Integer])
+    (display-name n)
     (value n))
   (nodeclass plus))
 (number 17)
