6d3bcbd910f2eb95abd4e7dc12edaf92ec304360
Ben Kovitz <bkovitz@indiana.edu>
2019-03-09 10:53:35 -0500

    Nodeclass inheritance partly implemented...

--- fargish.rkt
+++ fargish.rkt
@@ -1,9 +1,11 @@
 ; fargish.rkt  --  FARGish as a #lang? Implemented in Typed Racket?
 
-#lang typed/racket
+#lang debug typed/racket
 
 (require syntax/parse syntax/parse/define syntax/stx
-         (for-syntax "wheel.rkt" racket/syntax syntax/parse syntax/stx mischief/sort))
+         (for-syntax "wheel.rkt" racket/syntax syntax/parse syntax/stx
+                     syntax/id-table racket/function
+                     (only-in sugar slice-at) mischief/sort))
 (require "types.rkt" "typed-wheel.rkt")
 
 ; Generate make-node in the main macro?
@@ -20,7 +22,73 @@
   (define (attr-hash . args)
     (apply hasheq args))
 
-  (struct SortNodeclass (name parents) #:transparent)
+  (define (maybe-missing stx)
+    (if stx
+      stx
+      (syntax-property #'(void) 'missing? #t)))
+
+  (define (missing? stx)
+    (syntax-property stx 'missing?))
+
+  ; Compile-time Nodeclass
+  (struct CNodeclass (name parents attrs) #:transparent)
+  ; name : Symbol
+  ; parents : (Listof Symbol)
+  ; attrs : (Listof Syntax)
+
+  (define (items->fid-table ->k ->v items)
+    (for/fold ([ft (make-immutable-free-id-table)])
+              ([item items])
+      (free-id-table-set ft (->k item) (->v item))))
+
+  (define (make-CNodeclass name parents . attrs)
+    (let ([attr-pairs (for/list ([kv (slice-at attrs 2)]
+                                 #:when (not (missing? (cadr kv))))
+                        kv)])
+      (CNodeclass name
+                  parents
+                  (items->fid-table car cadr attr-pairs))))
+
+  (define (inherit-CNodeclass parent-cn cn)
+    (struct-copy CNodeclass cn
+                 [attrs (fidtable-merge (CNodeclass-attrs parent-cn)
+                                        (CNodeclass-attrs cn))]))
+
+  (define (fidtable-merge ft0 ft1)
+    (for/fold ([ft0 ft0])
+              ([k (free-id-table-keys ft1)])
+      (free-id-table-set ft0 k (free-id-table-ref ft1 k))))
+
+  ;(: merge-from-ancestors (All (A) A (-> A (Listof A)) (-> A A A) -> A))
+  (define (merge-from-ancestors item item->parents parent+item->item)
+    (let ([ancestor-seq (topological-sort (list item) item->parents)])
+      (cond
+        [(null? ancestor-seq) item]
+        [else (let loop ([parent (car ancestor-seq)]
+                         [ancestor-seq (cdr ancestor-seq)])
+                (cond
+                  [(null? ancestor-seq) (parent+item->item parent item)]
+                  [else (loop (parent+item->item parent (car ancestor-seq))
+                              (cdr ancestor-seq))]))])))
+
+  (define (nodeclass-inheritance cnodeclasses)
+    (let ([name->cnodeclass (let ([ft (items->fid-table CNodeclass-name
+                                                        identity
+                                                        cnodeclasses)])
+                              (λ (name) (free-id-table-ref ft name)))]
+          [cn->parents (λ (cn)
+                         (map name->cnodeclass
+                              (stx->list (CNodeclass-parents cn))))]
+          [cn->updated
+            (λ (cn) (merge-from-ancestors cn cn->parents inherit-CNodeclass))])
+      (for/list ([cn cnodeclasses])
+        (let ([cn (cn->updated cn)])
+          (with-syntax ([name (CNodeclass-name cn)]
+                        [parents (CNodeclass-parents cn)]
+                        [((attr-name attr-expr) ...) (free-id-table-map
+                                                       (CNodeclass-attrs cn)
+                                                       (λ kv kv))])
+            #'(name parents ((attr-name attr-expr) ...)))))))
 
 ;  (define (get-parents nc)
 ;    (with-syntax ([nc nc])
@@ -58,20 +126,25 @@
   (define-syntax-class nodeclass
     #:description "nodeclass"
     #:attributes [name (arg 1) (arg.type 1) (parent 1) value display-name
-                  tagclass? sortable]
+                  tagclass? cnodeclass]
     (pattern (head:nodeclass-head ~! decl:name+args body:nodeclass-body)
              #:with name #'decl.name
              #:with (arg ...) #'(decl.arg ...)
              #:with (arg.type ...) #'(decl.arg.type ...)
              #:with (parent ...) #'(body.parent ... ...)
-             #:attr sortable (SortNodeclass (syntax-e #'name) (map syntax-e (stx->list #'(parent ...))))
+             ;#:attr sortable (SortNodeclass (syntax-e #'name) (map syntax-e (stx->list #'(parent ...))))
              #:with value #'(~? body.value-expr #f)
              #:with display-name
                       (if (syntax-property #'body.display-name-expr 'missing?)
                         #''name
                         #'body.display-name-expr)
              #:with tagclass? #'head.tagclass?
-             #:do [(println (attribute sortable))]
+             #:attr cnodeclass
+                      (make-CNodeclass #'name
+                                       #'(parent ...)
+                                       #'vlue #'body.value-expr
+                                       #'dsplay-name #'body.display-name-expr)
+             ;#:do [(println (attribute sortable))]
 ;             #:do [(println (list (syntax->datum #'tagclass?)
 ;                                  (void? (syntax->datum #'tagclass?))))]
     ))
@@ -83,7 +156,7 @@
       (~alt (is-a ~! parent:expr ...+)
             (~optional (value ~! value-expr:expr)
                        #:too-many "'value' specified more than once"
-                       #:defaults ([value
+                       #:defaults ([value-expr
                                      (syntax-property #'(void) 'missing? #t)]))
             (~optional (display-name ~! display-name-expr:expr)
                        #:too-many "'display-name' specified more than once"
@@ -110,15 +183,9 @@
 ;                                    ;#'(nc.parent ...)))
 ;                          #'((list nc.parent ...) ...))
 ;     ;#:attr topo #'(nc.parent ...)
-     #:attr topo (attribute nc.sortable)
-     #:attr htopo (for/hash ([s (attribute topo)])
-                    (values (SortNodeclass-name s) (SortNodeclass-parents s)))
+     #:attr topo (attribute nc.cnodeclass)
      #:do [(displayln (format "topo: ~a" (attribute topo)))
-           ;(displayln (SortNodeclass-parents (cadr (attribute topo))))
-           ;(displayln (map SortNodeclass-name (attribute topo)))
-           ;(displayln (attribute htopo))
-           (displayln (topological-sort (hash-keys (attribute htopo))
-                                        (hash->f (attribute htopo))))]
+           (displayln (nodeclass-inheritance (attribute nc.cnodeclass)))]
      #`(begin
          (begin (: nc.name : nc.arg.type ... -> (Hashof Symbol Any))
                 (define (nc.name nc.arg ...)
@@ -154,10 +221,12 @@
 ;(number 17)
 
 (define-spec spec
-  (nodeclass A)
+  (nodeclass A
+    (display-name 'this-is-A))
   (nodeclass B
     (is-a A))
   (nodeclass C
-    (is-a A))
+    (is-a A)
+    (display-name 'this-is-C))
   (nodeclass D
     (is-a C)))
