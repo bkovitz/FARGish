98a944dec5038c07f8a0d664291820c809ae7ff4
Ben Kovitz <bkovitz@indiana.edu>
2019-03-28 19:41:00 -0400

    A force-directed graph layout copied from the web
    In web.rkt.

--- xweb.rkt
+++ xweb.rkt
@@ -1,43 +1,19 @@
-#lang debug at-exp racket
+; Experiment: simple web server
+
+#lang debug racket
 
 (require web-server/servlet
-         web-server/servlet-env
-         txexpr)
+         web-server/servlet-env)
  
 (define count 0)
-
-; Like response/xexpr but doesn't escape the bodies of 'script' and 'style'
-; tags. How does anyone live without this?
-(define (response/x xexpr)
-  (response/output (λ (output-port)
-                     (write-string (xexpr->html xexpr) output-port)
-                     (void))))
 
 (define (my-app req)
   (set! count (add1 count))
   (let ([c (format "~a" count)])
-    (response/x
+    (response/xexpr
      `(html (head (title "Hello world!"))
             (body 
-              (style "
-.links line {
-  stroke: #999;
-  stroke-opacity: 0.6;
-}
-
-.nodes circle {
-  stroke: #fff;
-  stroke-width: 1.5px;
-}
-
-text {
-  font-family: sans-serif;
-  font-size: 10px;
-}")
-              (svg ((width "960") (height "600")))
               (script ((src "jquery.js")))
-              (script ((src "https://d3js.org/d3.v4.min.js")))
-              (script ((type "text/javascript") (src "forcedirected2.js")))
               (script ((type "text/javascript"))
                 "function do_button() {
                    $.get(\"count\",
