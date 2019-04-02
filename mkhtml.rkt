adcf89263555076830eedb3f042649f0695e4018
Ben Kovitz <bkovitz@indiana.edu>
2019-04-02 14:03:30 -0400

    force-interactive demo
    Called from fargish.html. ('make fargish.html')

--- mkhtml.rkt
+++ mkhtml.rkt
@@ -23,15 +23,18 @@
 (define html
   `(html
      (head (title "FARG model")
-           (link ((rel "stylesheet") (type "text/css") (href "fargish.css"))))
+           #;(link ((rel "stylesheet") (type "text/css") (href "fargish.css")))
+           (link ((rel "stylesheet") (type "text/css") (href "force-interactive.css")))
+           )
      (body
        (svg ((id "ws") (width "960") (height "600"))
-         ,@temporal-trace
+         ;,@temporal-trace
          )
        (script ((src "jquery.js")))
-       (script ((src "https://d3js.org/d3.v4.min.js")))
+       (script ((src "//d3js.org/d3.v3.min.js")))
+       ;(script ((src "https://d3js.org/d3.v4.min.js")))
        ;(script ((src "force-directed3.js")))
-       
+       (script ((src "force-interactive.js")))
        )))
 
 (with-output-to-file "fargish.html" #:mode 'text #:exists 'replace
