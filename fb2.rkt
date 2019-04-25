eed4be7a0ca0032d2e1765fb7031a2ee67c4e667
Ben Kovitz <bkovitz@indiana.edu>
2019-04-25 15:33:44 -0400

    Trying the FARGish grammar with brag
    I'm liking it better this way. Definitely simpler and more readable.
    The syntax objects in the future still look a little scary, but not
    too much.
    
    fb2.rkt refers to fb.brag

---
+++ fb2.rkt
@@ -0,0 +1,47 @@
+#lang debug racket
+
+(require brag/support br-parser-tools/lex
+         "fb.brag")
+
+(define (tokenize ip)
+  (port-count-lines! ip)
+  (define my-lexer
+    (lexer-src-pos
+      [(char-set ":;,.{}[]()=") lexeme] ; single-character tokens
+      [(:or (char-set "+-*/<>") "==" "<=" "!=" ">=" "&&" "||")
+       (token 'BINARY-OP lexeme)]
+      ["--" lexeme]
+      ["nodeclass" (token 'NODECLASS (hash))]  ; empty hash => not a tag
+      ["tagclass" (token 'NODECLASS (hash 'tag? #t))]
+      ["codelet" 'CODELET]
+      ["me" 'ME]
+      ["STUB" 'STUB]
+      [#\" (token 'STRING (list->string (get-string-token input-port)))]
+      [(:: identifier-initial (:* identifier-subsequent))
+       (token 'IDENTIFIER (string->symbol lexeme))]
+      [whitespace (return-without-pos (my-lexer ip))]
+      [(eof) 'EOF]))
+  (thunk (my-lexer ip)))
+
+(define get-string-token
+  (lexer
+   [(:~ #\" #\\) (cons (car (string->list lexeme))
+                       (get-string-token input-port))]
+   [(:: #\\ #\\) (cons #\\ (get-string-token input-port))]
+   [(:: #\\ #\") (cons #\" (get-string-token input-port))]
+   [#\" null]))
+
+(define-lex-abbrevs
+  [identifier-punctuation (char-set "!@$%^&*-_+=|<>?/")]
+  [identifier-initial (:or alphabetic identifier-punctuation)]
+  [identifier-subsequent (:or identifier-initial numeric)])
+
+(define (lx str)
+  (let ([th (tokenize (open-input-string str))])
+    (let loop ([tok (th)])
+      (displayln tok)
+      (when (not (eq? 'EOF (srcloc-token-token tok)))
+        (loop (th))))))
+
+(define (p str)
+  (parse (tokenize (open-input-string str))))
