2bed8d502cf74298252b40b5cbe2a90929e9aff1
Ben Kovitz <bkovitz@indiana.edu>
2019-04-29 03:17:33 -0400

    A little bit with the syntax objects...

--- fb2.rkt
+++ fb2.rkt
@@ -81,19 +81,80 @@ nodeclass blah;
 (define s7 "nodeclass a() : b; nodeclass c;")
 (define s8 "nodeclass a(n : Integer) : b; nodeclass c;")
 (define s9 "nodeclass a(n : Integer) : b { blah = yah; } nodeclass c;")
+(define s10 "nodeclass a(x) : b, c { value = x; }")
 
+;; ======================================================================
+;;
+;; Making something out of the syntax objects from the parser
+;;
+
+; name : Symbol
+; default-attrs : (Hashof Symbol Any)
+; args : (Listof Arg)
+; ancestors : (Listof Symbol)
+; body :
+(struct Nodeclass (name default-attrs args ancestors body)
+                  #:prefab)
+(struct Arg (name type)
+            #:prefab)
+(struct Nodeclass-Body-Elem (lhs rhs)
+        #:prefab)
 
 (define (f fargish-spec-stx)
   (syntax-parse fargish-spec-stx
-    [(_ (elem ...))
+    [(_ elem ...)
      #'(list elem ...)]))
 
 (define-syntax (nodeclass-definition nodeclass-stx)
   (syntax-parse nodeclass-stx
-    [(_ ht name- args- ancestors- body-)
-     #'(hash-set* ht 'name 'name-
-                     'args 'args-
-                     'ancestors 'ancestors-
-                     'body 'body-)]))
+    [(_ ht name args ancestors body)
+     #'(Nodeclass 'name ht args ancestors 'body)]))
+
+(define-syntax (args stx)
+  (syntax-parse stx
+    [(_) #''()]
+    [(_ (arg ...))
+     #'(list arg ...)]))
+
+(define-syntax (untyped-arg stx)
+  (syntax-parse stx
+    [(_ id:id) #'(Arg 'id 'Any)]))
+
+(define-syntax (typed-arg stx)
+  (syntax-parse stx
+    [(_ id:id typename:expr)
+     #'(Arg 'id 'typename)]))
+
+(define-syntax (ancestors stx)
+  (syntax-parse stx
+    [(_ id:id ...) #'(list 'id ...)]))
+
+(define-syntax (nodeclass-body stx)
+  (syntax-parse stx
+    [(_ elem:expr ...) #'(list elem ...)]))
+
+(define-syntax (nodeclass-body-elem stx)
+  (syntax-parse stx
+    [(_ lhs:expr rhs:expr)
+     #'(Nodeclass-Body-Elem 'lhs 'rhs)]))
+
+
+;------ Throwaway test code
+
+(define a (f (p s4)))
+(displayln a)
+(define b (eval a))
+b
+(newline)
+(define aa (f (p s8)))
+(displayln aa)
+(define bb (eval aa))
+bb
+(newline)
+(define a1 (f (p s10)))
+(displayln a1)
+(define b1 (eval a1))
+b1
+
 
 
