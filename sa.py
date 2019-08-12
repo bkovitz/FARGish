faaa7438969c6a069f38f34354e4ccae7a17e3c0
Ben Kovitz <bkovitz@indiana.edu>
2019-08-12 12:52:01 -0400

    clean-up
    We can now remove edges between ports and find neighbors at a port.
    
    NEXT: a unit test.

--- sa.py
+++ sa.py
@@ -1,4 +1,4 @@
-# Daves's and Ben's spreading-activation experiment on port graphs
+# Dave's and Ben's spreading-activation experiment on port graphs
 
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -8,31 +8,6 @@ import statistics as stats
 from random import random, randint
 from collections import defaultdict, namedtuple
 import timeit
-
-class PortGraph0(nx.MultiGraph):
-    def __init__(self, *args, **kwargs):
-        self.ports = {}
-        super(PortGraph, self).__init__(*args, **kwargs)
-
-    def add_node(self, node_for_adding, **attr):
-        self.ports[node_for_adding] = {}
-        super(PortGraph, self).add_node(node_for_adding, **attr)
-
-    def add_edge(self, node1, port_label1, node2, port_label2, **attr):
-        if not node1 in self.ports:
-            self.add_node(node1)
-        if not port_label1 in self.ports[node1]:
-            self.ports[node1] = set()
-        key = super(PortGraph, self).add_edge(node1, node2, **attr)
-        labels = self.ports[node1].get(port_label1, set())
-        labels.add(frozenset([node1, node2, key]))
-
-        if not node2 in self.ports:
-            self.add_node(node2)
-        if not port_label2 in self.ports[node2]:
-            self.ports[node2] = {}
-        labels = self.ports[node2].get(port_label2, set())
-        labels.add(frozenset([node1, node2, key]))
 
 empty_set = frozenset()
 
@@ -85,14 +60,6 @@ class PortGraph(nx.MultiGraph):
                 self._remove_pni(node1, port_label1, pni)
             for pni in pnis2:
                 self._remove_pni(node2, port_label2, pni)
-
-#            edge_keys1 = set(self.edge_keys(node1, port_label1))
-#            edge_keys2 = set(self.edge_keys(node2, port_label2))
-#            print(edge_keys1.intersection(edge_keys2))
-#            for key in edge_keys1.intersection(edge_keys2):
-#                super().remove_edge(node1, node2, key)
-#                edge_keys1.remove(key)
-#                edge_keys2.remove(key)
 
     def neighbors(self, node, port_label=None):
         if port_label is None:
