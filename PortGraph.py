530ae9e8c169f16974778ab1c4efb7d7d212a5d7
Ben Kovitz <bkovitz@indiana.edu>
2019-08-13 15:07:26 -0400

    Spreading activation
    Spreading activation operates on dictionaries, and is called with
    functions to indicate neighbors and edge weights.
    
    PortGraph now moved to PortGraph.py.
    
    Still need unit tests for both.

---
+++ PortGraph.py
@@ -0,0 +1,81 @@
+# PortGraph.py -- PortGraph class
+
+import networkx as nx
+
+from collections import defaultdict, namedtuple
+
+empty_set = frozenset()
+
+PortNeighborInfo = namedtuple('PortNeighborInfo',
+    ['neighbor', 'neighbor_port_label', 'edge_key']
+)
+
+class PortGraph(nx.MultiGraph):
+
+    def add_node(self, node_for_adding, **attr):
+        attr['_ports'] = defaultdict(set) # {port_label: set(PortNeighborInfo)}
+        super(PortGraph, self).add_node(node_for_adding, **attr)
+
+    def add_edge(self, node1, port_label1, node2, port_label2, **attr):
+        if node1 not in self:
+            self.add_node(node1)
+        if node2 not in self:
+            self.add_node(node2)
+        key = super(PortGraph, self).add_edge(node1, node2, **attr)
+        self.nodes[node1]['_ports'][port_label1].add(
+            PortNeighborInfo(node2, port_label2, key)
+        )
+        self.nodes[node2]['_ports'][port_label2].add(
+            PortNeighborInfo(node1, port_label1, key)
+        )
+        return key
+
+    def pnis(self, node, port_label):
+        'Returns set of PortNeighborInfos for node.port_label'
+        return self.nodes[node]['_ports'].get(port_label, empty_set)
+
+    def _remove_pni(self, node, port_label, pni):
+        self.nodes[node]['_ports'][port_label].discard(pni)
+
+    def edge_keys(self, node, port_label):
+        'Returns iterator over edge keys from node.port_label'
+        return (i.edge_key for i in
+            self.nodes[node]['_ports'].get(port_label, []))
+
+    def remove_edge(self, node1, port_label1, node2, port_label2):
+        if node1 in self and node2 in self:
+            pnis1 = [pni for pni in self.pnis(node1, port_label1)
+                            if pni.neighbor == node2 and
+                               pni.neighbor_port_label == port_label2]
+            pnis2 = [pni for pni in self.pnis(node2, port_label2)
+                            if pni.neighbor == node1 and
+                               pni.neighbor_port_label == port_label1]
+            for pni in pnis1:
+                super().remove_edge(node1, node2, pni.edge_key)
+                self._remove_pni(node1, port_label1, pni)
+            for pni in pnis2:
+                self._remove_pni(node2, port_label2, pni)
+
+    def neighbors(self, node, port_label=None):
+        if port_label is None:
+            return super().neighbors(node)
+        else:
+            return (i.neighbor for i in self.pnis(node, port_label))
+
+
+if __name__ == '__main__':
+    g = PortGraph()
+    g.add_edge('A', 'in', 'B', 'out')
+    g.add_edge('O', 'members', 'A', 'member_of')
+    #print(g.nodes(data=True))
+    print(list(g.neighbors('A')))
+    print(list(g.neighbors('A', 'in')))
+    print(list(g.neighbors('B', 'out')))
+    g.remove_edge('A', 'in', 'B', 'out')
+    print(list(g.neighbors('A')))
+    print(list(g.neighbors('A', 'in')))
+    print(list(g.neighbors('B', 'out')))
+
+    #print(g.adj)
+    #print(list(g.adjacency()))
+    #g.remove_edge('A', 'in', 'B', 'out')
--- sa.py
+++ sa.py
@@ -1,4 +1,4 @@
-# Dave's and Ben's spreading-activation experiment on port graphs
+# sa.py -- Spreading activation
 
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -8,78 +8,60 @@ import statistics as stats
 from random import random, randint
 from collections import defaultdict, namedtuple
 import timeit
+from math import exp
 
-empty_set = frozenset()
+def sa(d_t0, neighbors, edge_weight, T, decay=1.0, num_steps=10):
+    '''Generic spreading activation
 
-PortNeighborInfo = namedtuple('PortNeighborInfo',
-    ['neighbor', 'neighbor_port_label', 'edge_key']
-)
+    d_t0 = dict of node -> activation at time t0
+    neighbors = function(node) -> neighbors spreading activation to node
+    edge_weight = function(from_node, to_node) -> edge weight
+    T = transfer function
+    decay = multiplier for each node's activation before passing to T
+    num_steps = number of timesteps
 
-class PortGraph(nx.MultiGraph):
+    Returns dict of node -> activation after num_steps of spreading
+    activation.'''
+    d_t1 = d_t0
+    for i in range(num_steps):
+        d_t0, d_t1 = d_t1, {}
+        for node, activation in d_t0.items():
+            d_t1[node] = T(d_t0[node] * decay
+                           +
+                           sum(d_t0[neighbor] * edge_weight(neighbor, node)
+                               for neighbor in neighbors(node)
+                           )
+                         )
+    return d_t1
 
-    def add_node(self, node_for_adding, **attr):
-        attr['_ports'] = defaultdict(set) # {port_label: set(PortNeighborInfo)}
-        super(PortGraph, self).add_node(node_for_adding, **attr)
+def activations(g):
+    d_t0 = {}
+    for node in g.nodes:
+        d_t0[node] = g.nodes[node].get('a', 0.0)
+    return d_t0
 
-    def add_edge(self, node1, port_label1, node2, port_label2, **attr):
-        if node1 not in self:
-            self.add_node(node1)
-        if node2 not in self:
-            self.add_node(node2)
-        key = super(PortGraph, self).add_edge(node1, node2, **attr)
-        self.nodes[node1]['_ports'][port_label1].add(
-            PortNeighborInfo(node2, port_label2, key)
-        )
-        self.nodes[node2]['_ports'][port_label2].add(
-            PortNeighborInfo(node1, port_label1, key)
-        )
-        return key
+def set_activations(g, d):
+    for node, activation in d.items():
+        g.nodes[node]['a'] = activation
 
-    def pnis(self, node, port_label):
-        'Returns set of PortNeighborInfos for node.port_label'
-        return self.nodes[node]['_ports'].get(port_label, empty_set)
-
-    def _remove_pni(self, node, port_label, pni):
-        self.nodes[node]['_ports'][port_label].discard(pni)
-
-    def edge_keys(self, node, port_label):
-        'Returns iterator over edge keys from node.port_label'
-        return (i.edge_key for i in
-            self.nodes[node]['_ports'].get(port_label, []))
-
-    def remove_edge(self, node1, port_label1, node2, port_label2):
-        if node1 in self and node2 in self:
-            pnis1 = [pni for pni in self.pnis(node1, port_label1)
-                            if pni.neighbor == node2 and
-                               pni.neighbor_port_label == port_label2]
-            pnis2 = [pni for pni in self.pnis(node2, port_label2)
-                            if pni.neighbor == node1 and
-                               pni.neighbor_port_label == port_label1]
-            for pni in pnis1:
-                super().remove_edge(node1, node2, pni.edge_key)
-                self._remove_pni(node1, port_label1, pni)
-            for pni in pnis2:
-                self._remove_pni(node2, port_label2, pni)
-
-    def neighbors(self, node, port_label=None):
-        if port_label is None:
-            return super().neighbors(node)
-        else:
-            return (i.neighbor for i in self.pnis(node, port_label))
+def simple_sa(g, decay=1.0, num_steps=10):
+    def neighbors(node):
+        return g.neighbors(node)
+    def edge_weight(from_node, to_node):
+        return 1.0
+    def T(x):
+        return 2.0 / (1 + exp(-2.2 * x)) - 1.0
+    return sa(activations(g), neighbors, edge_weight, T,
+              decay=decay, num_steps=num_steps
+           )
 
 
 if __name__ == '__main__':
+    from PortGraph import PortGraph
     g = PortGraph()
-    g.add_edge('A', 'in', 'B', 'out')
-    g.add_edge('O', 'members', 'A', 'member_of')
-    #print(g.nodes(data=True))
-    print(list(g.neighbors('A')))
-    print(list(g.neighbors('A', 'in')))
-    print(list(g.neighbors('B', 'out')))
-    g.remove_edge('A', 'in', 'B', 'out')
-    print(list(g.neighbors('A')))
-    print(list(g.neighbors('A', 'in')))
-    print(list(g.neighbors('B', 'out')))
-    #print(g.adj)
-    #print(list(g.adjacency()))
-    #g.remove_edge('A', 'in', 'B', 'out')
+    g.add_edge('A', 'sa', 'B', 'sa')
+    g.add_edge('O', 'sa', 'A', 'sa')
+
+    g.nodes['A']['a'] = 1.0
+    set_activations(g, simple_sa(g))
+    print(activations(g))
