5d1bc1b1454d01a97808bf3fc0b42ffa9b22a8b3
Ben Kovitz <bkovitz@indiana.edu>
2021-10-18 17:09:24 -0400

    CellRef

--- new/Canvas.py
+++ new/Canvas.py
@@ -58,15 +58,75 @@ class Canvas(ABC):
         pass
 
 @dataclass(frozen=True)
-class CellRef:  #TODO (HasAvailValues):
+class CellRef(HasAvailValues):
+    '''A reference to a cell in a Canvas.'''
     canvas: Union[Canvas, None] = None
     addr: Union[Addr, None] = None
 
+    # TODO .next_cellref(), .last_nonblank_cellref(), cellrefs_forward()
+
+    @property
+    def value(self) -> Value:
+        if (
+            self.canvas is not None
+            and
+            self.addr is not None
+        ):
+            return self.canvas[self.addr]
+        else:
+            return None
+
+    def has_a_value(self) -> bool:
+        return self.value is not None
+
+    def paint(self, v: Value) -> None:
+        if (  # OAOO this if?  (might be hard with mypy)
+            self.canvas is not None
+            and
+            self.addr is not None
+        ):
+            self.canvas[self.addr] = v
+
+    @property
+    def avails(self) -> Union[Sequence[Value], None]:
+        if isinstance(self.value, HasAvailValues):
+            return self.value.avails
+        else:
+            return None
+
+    def take_avails(self, values: Iterable[Value]) \
+    -> Tuple[Iterable[Value], Iterable[Value]]:
+        v = self.value
+        if isinstance(v, HasAvailValues):
+            try:
+                return v.take_avails(values)
+            except ValuesNotAvail as vna:
+                raise replace(vna, cellref=self)
+        else:
+            raise ValuesNotAvail(self, tuple(values), ())
+
+    def __str__(self):
+        return f'canvas[{self.addr}]'
+
+@dataclass(frozen=True)
+class StepDelta:
+    '''A change from one Step to another.'''
+    before: Any    # What got replaced
+    after: Any     # What replaced it
+    how: Union[Any, None]   # Some clue about how it happened
+
+    def seq_str(self):
+        '''How to display this inside SeqState.__str__.'''
+        return str(self)
+
 @dataclass(frozen=True)
 class Step(HasAvailValues):
+    '''One *state* in a sequence of steps, i.e. a StepCanvas. The actual change
+    that happened during one step is a StepDelta. The first ('zeroth') step in
+    the sequence normally has no last_delta.'''
     avails: Union[Sequence[Value], None] = None
+    last_delta: Union[StepDelta, None] = None
 
-    # TODO Put this into a WithAvails mix-in.
     def take_avails(self, values: Iterable[Value]) \
     -> Tuple[Iterable[Value], Iterable[Value]]:
         '''Returns (taken_avails, remaining_avails). Might raise
@@ -91,3 +151,39 @@ class Step(HasAvailValues):
                 tuple(missing_avails)
             )
         return (taken_avails, remaining_avails)
+
+@dataclass(eq=False)
+class StepCanvas(Canvas):
+    '''A Canvas consisting of a sequence of Steps. A StepCanvas is mutable
+    (as painters paint on it). It is possible for two StepCanvases in the same
+    workspace to be identical, so equality comparison is only an id()
+    comparison.'''
+    steps: List[Union[Step, None]] = field(default_factory=list)
+    
+    def __hash__(self):
+        return id(self)
+
+    def __getitem__(self, addr: Addr) -> Value:
+        # TODO Handle addr that can't be found or is not an index
+        #print('SEQCGET', addr, len(self.steps))
+        if isinstance(addr, int) and addr < len(self.steps):
+            return self.steps[addr]
+        else:
+            return None
+
+    def __setitem__(self, addr: Addr, v: Value):
+        # TODO Handle addr that doesn't work as a list index
+        # TODO Accept a builder argument?
+        if isinstance(addr, int):
+            while len(self.steps) <= addr:
+                self.steps.append(None)
+            self.steps[addr] = v  # type: ignore[call-overload]  # mypy bug?
+        else:
+            raise NotImplementedError(f'addr must be int; was {type(addr)}: {addr}')
+
+    def __len__(self):
+        return len(self.steps)
+
+    def __str__(self):
+        cl = self.__class__.__name__
+        return f"{cl}({'; '.join(str(st) for st in self.steps)})"
--- new/testCanvas.py
+++ new/testCanvas.py
@@ -6,7 +6,9 @@ import inspect
 
 from dataclasses import dataclass, field
 
-from Canvas import Step, ValuesNotAvail
+from Canvas import CellRef, Step, StepDelta, ValuesNotAvail, StepCanvas
+from util import as_iter
+
 
 class TestStepsCanvas(unittest.TestCase):
 
@@ -28,3 +30,53 @@ class TestStepsCanvas(unittest.TestCase):
             cm.exception,
             ValuesNotAvail(None, (4, None), (None, 7))
         )
+
+    def test_stepcanvas_basics(self) -> None:
+        ca = StepCanvas([Step([4, 5, 6])])
+        self.assertEqual(ca[0], Step([4, 5, 6]))
+        self.assertEqual(ca[1], None)
+        self.assertEqual(ca[10], None)
+        self.assertEqual(len(ca), 1)
+
+        ca[1] = Step([6], StepDelta([4, 5], [9], '+'))
+        self.assertEqual(ca[1], Step([6], StepDelta([4, 5], [9], '+')))
+        self.assertEqual(len(ca), 2)
+
+    def test_cellref_basics(self) -> None:
+        ca = StepCanvas([Step([4, 5, 6])])
+        cr0 = CellRef(ca, 0)
+        cr1 = CellRef(ca, 1)
+
+        # StepCanvas[0]
+        self.assertEqual(cr0.value, Step([4, 5, 6]))
+        self.assertCountEqual(as_iter(cr0.avails), [4, 5, 6])
+        taken, remaining = cr0.take_avails([4, 5])
+        self.assertCountEqual(taken, [4, 5])
+        self.assertCountEqual(remaining, [6])
+        self.assertEqual(cr0.value, Step([4, 5, 6]))
+        self.assertTrue(cr0.has_a_value())
+
+        self.assertTrue(cr0.has_avail_value(4))
+        self.assertFalse(cr0.has_avail_value(7))
+
+        with self.assertRaises(ValuesNotAvail) as cm:
+            taken, remaining = cr0.take_avails([4, 7])
+        self.assertEqual(
+            cm.exception,
+            ValuesNotAvail(cr0, (4, None), (None, 7))
+        )
+
+        #StepCanvas[1]
+        self.assertEqual(cr1.value, None)
+        self.assertFalse(cr1.has_a_value())
+        self.assertCountEqual(as_iter(cr1.avails), [])
+        with self.assertRaises(ValuesNotAvail) as cm:
+            taken, remaining = cr1.take_avails([4, 7])
+        self.assertEqual(
+            cm.exception,
+            ValuesNotAvail(cr1, (4, 7), ())
+        )
+
+        # paint
+        cr1.paint(Step([6], StepDelta([4, 5], [9], '+')))
+        self.assertEqual(ca[1], Step([6], StepDelta([4, 5], [9], '+')))
