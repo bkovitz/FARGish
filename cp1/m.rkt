06bad26f27e15ce34ea1acc57b506cefff4862a9
Ben Kovitz <bkovitz@indiana.edu>
2022-07-11 15:25:16 -0400

    A QPainter finally type-checks in Racket
    (define qp : Painter
      `((x (+ x 2) ,same) working-soup (x (+ x 1) j)))
    
    Setting up the types for this was hard!

--- cp1/m.rkt
+++ cp1/m.rkt
@@ -6,6 +6,8 @@
 
 (struct Det-Addr ([container : Container] [index : Index]) #:transparent)
 
+(define-type Container-Ref (U 'working-soup 'main-canvas))
+
 (define-type Container (U Canvas Soup))
 
 (struct Canvas ([s : String] [clarities : (Mutable-Vectorof Integer)])
@@ -18,15 +20,16 @@
 
 (define-type Painter (List Addr Addr Func))
 
-(define-type Func (-> Value Value))
+(define-type Func (U (-> Value (U Value Fizzle)) Value Painter-Template))
 
 (define-type Value (U Integer Char Painter Func))
 
-(define-type Addr (U Index Soup Painter-Template Det-Addr))
+(define-type Addr (U Index Soup Container-Ref Painter-Template Det-Addr))
 
 (define-type Painter-Template (List Addr-Expr Addr-Expr Func))
 
-(define-type Addr-Expr (U Addr))  ; TODO (+ 'I 1), etc.
+;(define-type Addr-Expr (U Addr))  ; TODO (+ 'I 1), etc.
+(define-type Addr-Expr (U Symbol Integer (List* '+ (Listof Addr-Expr))))
 
 (struct Env ([canvas : Canvas] [hash : (Immutable-HashTable Any Any)])
         #:transparent)
@@ -46,6 +49,15 @@
 (define d (Det-Addr c 2))
 
 (define e (Env c (hash)))
+
+(: succ Func)
+(define (succ x)
+  (cond
+    [(integer? x) (add1 x)]
+    [else (Fizzle)]))
+
+(: p Painter)
+(define p `(1 3 ,succ))
 
 ; The default (simplest) way to map an Addr to a Det-addr.
 ;(: eval-as/det-addr/default (-> Env (U Value Addr) (U Det-Addr Fizzle)))
@@ -79,3 +91,44 @@
 (: canvas/get-value (-> Canvas Index (U Char Fizzle)))
 (define (canvas/get-value c i)
   (string-ref (Canvas-s c) i))
+
+;(: eval-as (-> Env Symbol 
+;(define (eval-as env what-as expr)
+;  )
+
+; eval maps an expr to a value. For cp, we need an eval-as that can return
+; a new state of the system, where a pending action can generate zero or
+; more pending actions as well as modify a canvas.
+
+;(define (run-painter env painter)
+;  (match painter
+;    [(list source target func)
+;     
+;(define (run-painter env painter k)
+
+; Pass a continuation? What would use this? What result would we pass to
+; the continuation? Success and failure continuations?
+
+; In a way, painters are just builders. We could easily add scouts and
+; testers.
+
+(define same : Func
+  (λ ([v : Value]) : Value v))
+
+(define qp : Painter
+  `((x (+ x 2) ,same) working-soup (x (+ x 1) j)))
+
+(define fj : Func 'j)
+
+(define f : Func
+    '(x (+ x 1) j))
+
+(define p1 : Painter-Template
+  `(x (+ x 2) ,same))
+
+(define pp : Painter
+  `(,p1 working-soup j))
+
+(define-type L (List* '+ (Listof Symbol)))
+
+(define l : L '(+ a b c d))
