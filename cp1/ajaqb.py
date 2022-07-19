0d896efc92dc98086cb97a159ec0fa90bf60999c
Ben Kovitz <bkovitz@indiana.edu>
2022-07-18 20:21:46 -0400

    Just barely started ajaqb model

---
+++ cp1/ajaqb.py
@@ -0,0 +1,7 @@
+# ajaqb.py -- Just enough model to run the ajaqb test
+
+from typing import Any, Callable, ClassVar, Collection, Dict, FrozenSet, \
+    Hashable, IO, Iterable, Iterator, List, Literal, NewType, Optional, \
+    Protocol, Sequence, Sequence, Set, Tuple, Type, TypeVar, Union, \
+    runtime_checkable, TYPE_CHECKING
+
