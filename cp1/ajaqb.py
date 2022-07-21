edc0dcd14fa88fcab411b60d9d593f2193ae1250
Ben Kovitz <bkovitz@indiana.edu>
2022-07-20 21:28:05 -0400

    Started is_match()...

--- cp1/ajaqb.py
+++ cp1/ajaqb.py
@@ -3,12 +3,16 @@
 from __future__ import annotations
 from typing import Any, Callable, ClassVar, Collection, Dict, FrozenSet, \
     Hashable, IO, Iterable, Iterator, List, Literal, NewType, Optional, \
-    Protocol, Sequence, Sequence, Set, Tuple, Type, TypeVar, Union, \
+    Protocol, Sequence, Sequence, Set, Tuple, Type, TypeGuard, TypeVar, Union, \
     runtime_checkable, TYPE_CHECKING
 from dataclasses import dataclass, field, fields, replace, InitVar, Field
 from abc import ABC, abstractmethod
+from random import choice
+import operator
+from functools import reduce
 
-from util import Numeric, short, as_tuple, pts, force_setattr
+from util import Numeric, short, as_tuple, as_list, pts, force_setattr, union, \
+    reseed
 
 @dataclass
 class Soup:
@@ -17,15 +21,74 @@ class Soup:
     def add(self, p: Painter) -> None:
         self.painters.add(p)
 
+    def matching_painters(self, xp: Painter) -> List[Painter]:
+        return [
+            p for p in self.painters if self.is_match(xp, p)
+        ]
+
+    def is_match(self, xp: Painter, p: Painter) -> bool:
+        '''Viewing xp as a painter template (possibly with variables that
+        need to be filled in), does p match xp?'''
+        print(f'ISM xp={xp}  p={p}')
+        if xp == p:
+            return True
+        else:
+            xi, xj, xf = xp
+            pi = as_index(p[0])
+            pj = as_index(p[1])
+            pf = p[2]  # TODO Call as_func?
+
+            subst: Dict[Variable, Index] = {}
+
+
+            print(xi, xj, xf)
+            print(pi, pj, pf)
+            raise NotImplementedError
+
+    @classmethod
+    def union(cls, *soups: Soup) -> Soup:
+        return Soup(union(*(soup.painters for soup in soups)))
+
 class WorkingSoup(Soup):
     pass
 
+class HasAsIndex(ABC):
+
+    @abstractmethod
+    def as_index(self) -> Index:
+        pass
+
+@dataclass(frozen=True)
+class Variable(HasAsIndex):
+    name: str
+
+    def __str__(self) -> str:
+        return self.name
+
+    def as_index(self) -> Index:
+        raise Fizzle   # TODO Look this up in an Env
+
+I = Variable('i')
+J = Variable('j')
+
+@dataclass(frozen=True)
+class Plus(HasAsIndex):
+    args: Tuple[Expr]
+
+    def __init__(self, *args: Expr):
+        force_setattr(self, 'args', args)
+
+    def __str__(self) -> str:
+        return '+'.join(str(a) for a in self.args)
+
+    def as_index(self) -> Index:
+        return reduce(operator.add, (as_index(arg) for arg in self.args), 0)
 
 Value = Hashable
 Index = int   # the index of a cell within a Canvas
-Expr = Hashable   # Restrict this better?
-Addr = Union[Index, str, Type[WorkingSoup], Expr]
 Func = Union[Callable[[Value], Value], Value]
+Expr = Union[Variable, Plus, int, Func]  # TODO Move Func to Painter?
+Addr = Union[Index, str, Type[WorkingSoup], Expr]
 Painter = Tuple[Addr, Addr, Expr]
 
 def run_all() -> None:
@@ -33,10 +96,10 @@ def run_all() -> None:
     'a    '.'''
     m = Model()
     m.absorb('ajaqb')
-    #m.regen_from('a    ')
-    print(m.canvas)
     for p in m.lts.painters:
         print(painter_str(p))
+    m.regen_from('a    ')
+    print(f"'{short(m.canvas)}'")
 
 class Fizzle(Exception):
     pass
@@ -53,7 +116,12 @@ class CanvasAddr:
     canvas: Canvas
     index: Index
 
-DeterminateAddr = CanvasAddr
+    def __repr__(self) -> str:
+        return str(self.index)
+
+PaintableSoupRef = Type[WorkingSoup]
+PainterRef = Tuple[PaintableSoupRef, Painter]
+DeterminateAddr = Union[CanvasAddr, PaintableSoupRef, PainterRef]
 
 @dataclass(kw_only=True)  # type: ignore[call-overload, misc]
 class Canvas(ABC):
@@ -202,6 +270,13 @@ class Canvas1D(Canvas):
                 return CanvasAddr(self, i + 1)
         raise FizzleValueNotFound(v)
 
+    def all_matching(self, v: Value) -> List[CanvasAddr]:
+        return [
+            CanvasAddr(self, i + 1)
+                for i, x in enumerate(self.contents)
+                    if x == v
+        ]
+
     def __str__(self) -> str:
         items = ' '.join(short(x) for x in self.contents)
         citems = ' '.join(short(c) for c in self.clarities)
@@ -236,26 +311,6 @@ def pred(v: Value) -> Value:
     raise Fizzle
 
 
-@dataclass(frozen=True)
-class Variable:
-    name: str
-
-    def __str__(self) -> str:
-        return self.name
-
-I = Variable('i')
-J = Variable('j')
-
-@dataclass(frozen=True)
-class Plus:
-    args: Tuple[Expr]
-
-    def __init__(self, *args: Expr):
-        force_setattr(self, 'args', args)
-
-    def __str__(self) -> str:
-        return '+'.join(str(a) for a in self.args)
-
 def painter_str(p: Painter) -> str:
     i, j, func = p
     return f'({addr_str(i)}, {addr_str(j)}, {func_str(func)})'
@@ -267,8 +322,8 @@ def addr_str(a: Addr) -> str:
         return repr(a)
     elif a == WorkingSoup:
         return 'WorkingSoup'
-    elif isinstance(a, tuple):
-        return painter_str(a)  # type: ignore[arg-type]
+    elif is_painter(a):
+        return painter_str(a)
     elif isinstance(a, Variable):
         return a.name
     else:
@@ -277,10 +332,13 @@ def addr_str(a: Addr) -> str:
 def func_str(func: Func) -> str:
     if callable(func):
         return short(func)
-    elif isinstance(func, tuple):
-        return painter_str(func)  # type: ignore[arg-type]
+    elif is_painter(func):
+        return painter_str(func)
     else:
         return repr(func)
+
+def is_painter(x: Func) -> TypeGuard[Painter]:
+    return isinstance(x, tuple)
 
 @dataclass
 class Model:
@@ -292,7 +350,6 @@ class Model:
 
     def absorb(self, s: str) -> None:
         for i, j, func in self.find_related_cells(s):
-            #print(i, j, func)
             self.lts.add((s[i-1], WorkingSoup, (I, Plus(I, (j - i)), func)))
             if j - i == 2:  # if need a painter to put a letter between two
                             # letters
@@ -313,5 +370,84 @@ class Model:
                 elif ord(s[i]) == ord(s[j]) + 1:
                     yield (i+1, j+1, pred)
 
+    def regen_from(self, s: str) -> None:
+        '''Regenerates canvas from 's'.'''
+        self.set_canvas(s)
+        for t in range(1, 20):  # TODO replace arbitrary constant
+            p = self.choose_painter()
+            print(t, painter_str(p))
+            self.run_painter(p)
+
+    def set_canvas(self, s: str) -> None:
+        self.canvas = Canvas1D.make_from(s)
+        
+    def add_painter(self, pr: PainterRef) -> None:
+        ref, p = pr
+        if ref == WorkingSoup:
+            self.ws.add(p)
+        else:
+            raise NotImplementedError
+
+    def choose_painter(self) -> Painter:
+        return choice(as_list(Soup.union(self.lts, self.ws).painters))
+
+    def run_painter(self, p: Painter) -> None:
+        i, j, func = p
+        print(f'i = {i}')
+        I = self.eval_as_detaddr(i)
+        print(f'I = {I}\n')
+        # NEXT eval_as_detaddr(i)
+        #      eval_as_detaddr(j)
+        # get the value for i
+        # pass it through func
+        # paint it at j
+
+    def eval_as_detaddr(self, a: Addr) -> DeterminateAddr:
+        if isinstance(a, str):
+            addrs = self.canvas.all_matching(a)
+            if not addrs:
+                raise Fizzle
+            else:
+                return choice(addrs)
+        elif is_paintable_soup(a):
+            return a
+        elif is_painter(a):
+            # find painters that match a
+            painters = self.ws.matching_painters(a)
+            if not painters:
+                raise Fizzle
+            else:
+                return (WorkingSoup, choice(painters))
+        elif is_expr(a):
+            return CanvasAddr(self.canvas, as_index(a))
+#        elif # Expr
+#            # ?
+        else:
+            print(f'\na = {a}')
+            raise Fizzle
+
+def as_index(e: Expr) -> Index:
+    if isinstance(e, int):
+        return e
+    elif isinstance(e, HasAsIndex):
+        return e.as_index()
+    else:
+        raise Fizzle
+
+def is_paintable_soup(x: Any) -> TypeGuard[PaintableSoupRef]:
+    return x == WorkingSoup
+
+def is_expr(x: Any) -> TypeGuard[Expr]:
+    return isinstance(x, int) or hasattr('as_index', x)
+
 if __name__ == '__main__':
-    run_all()
+#    reseed(0)
+#    run_all()
+
+    m = Model()
+    m.set_canvas('ajaqb')
+    m.add_painter((WorkingSoup, (1, 3, same)))
+    pts(m.ws.painters)
+    print()
+    print(m.eval_as_detaddr((I, Plus(I, 2), same)))
+
