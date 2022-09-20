6cf22cf93630e52e9f9f13658ac8494759a0dec6
Ben Kovitz <bkovitz@indiana.edu>
2022-09-20 10:14:08 -0400

    Updated scratch.py: ContentAndClarity

--- cp2/scratch.py
+++ cp2/scratch.py
@@ -1,15 +1,23 @@
 
-
-
 @dataclass(frozen=True)
-class Annotation:
+class AnnotationType:
     name: str
 
     def __str__(self) -> str:
         return self.name
 
-Start = Annotation('Start')
-End = Annotation('End')
+Anchor = AnnotationType('Anchor')
+
+@dataclass(frozen=True)
+class Annotation:
+    type: AnnotationType
+    name: str
+
+    def __str__(self) -> str:
+        return self.name
+
+Start = Annotation(Anchor, 'Start')
+End = Annotation(Anchor, 'End')
 
 @dataclass(frozen=True)
 class Annotations:
@@ -30,8 +38,8 @@ class CellBundle:
         return f'CellBundle({short(self.value)}; {self.annotations.elems_str()})'
 
 
-CellContents = Union[CanvasValue, Annotation, Annotations, CellBundle]
-CellAddr = Union[Index, CellContents]
+CellContent = Union[CanvasValue, Annotation, Annotations, CellBundle]
+CellAddr = Union[Index, CellContent]
 
 
 
@@ -41,7 +49,27 @@ class ContentAndClarity:
     clarity: Numeric
 
     def paint(self, v: CellContent) -> None:
-        pass # TODO
+        # TODO None
+        if v is None:
+            self.dec_clarity()
+        elif v == self.content:
+            self.inc_clarity()
+        else:
+            if self.clarity == 0:
+                self.content = v
+                self.clarity = 1
+            else:
+                self.dec_clarity()
+
+    def inc_clarity(self) -> None:
+        if self.clarity < MAX_CLARITY:
+            self.clarity += 1
+
+    def dec_clarity(self) -> None:
+        if self.clarity > 0:
+            self.clarity -= 1
+        if self.clarity == 0:
+            self.content = None
 
 InternalIndex = Union[Index, Tuple[Index, AnnotationType]]
 
@@ -52,7 +80,7 @@ class ContentsAndClarities:
     max_index: Index
 
     def __setitem__(self, i: InternalIndex, v: CellContent) -> None:
-        if is_index(i):
+        if is_index(i):  # WRONG: need to extract index from i
             # check bounds
         if i in self.d:
             self.d[i].paint(v)
@@ -65,5 +93,24 @@ class ContentsAndClarities:
     def set_clarity(self, i: InternalIndex, clarity: Numeric) -> None:
         pass # TODO
 
+# TODO How do you erase an annotation?
+
+
+
+# In Canvas1D:
+    def __setitem__(self, i: Index, v: CellContent) -> None:
+        for ii, vv in self.as_internal_args(i, v):
+            self.d[ii] = vv
+
+    def as_internal_args(self, i: Index, v: CellContent) \
+    -> Iterable[Tuple[InternalIndex, Union[CanvasValue, Annotation]]:
+        match v:
+            case str():
+                yield (i, v)
+            case Annotation():
+                yield ((i, v.type), v)
+            case Annotations():
+                for elem in v:
+                    yield ((i, elem.type), elem)
 
 contents: Dict[Index, ContentsAndClarities]
