a946cce3924a4220da768a3e1490e0809cb73230
Ben Kovitz <bkovitz@indiana.edu>
2022-12-01 19:03:43 -0500

    More tentative code in spike.py

--- cp3/spike.py
+++ cp3/spike.py
@@ -17,6 +17,12 @@ class Succ:
             else:
                 yield ctx
 
+    def unify(self, ctx: Context) -> Iterable[Context]:
+        for ii, jj in ctx.all_addr_pairs(self.i, self.j):
+            match (ctx.astatus(ii), ctx.astatus(jj)):
+                case (AS.Filled, AS.Filled):
+                    yield from ctx.unify(self.i, ii).unify(self.j, jj)
+        
 
 @dataclass(frozen=True)
 class Apart:
@@ -33,3 +39,39 @@ class Apart:
                        .unify(self.i, ii)
                        .unify(self.j, jj)
                 )
+
+
+class AS:
+    '''Just a holder for AddrStatuses to make them easy to refer to in 'case'
+    statements.'''
+    Blank = Atom('Blank')
+    Filled = Atom('Filled')
+    Nonexistent = Atom('Nonexistent)'
+
+AddrStatus = Literal[AS.Blank, AS.Filled, AS.Nonexistent]
+
+
+@dataclass(frozen=True)
+class Context:
+    subst: Subst
+    ws: WorkspaceState
+    actions: Set[Action]
+
+    def unify(self, e1: Expr, e2: Expr) -> Context:
+        pass
+
+    def paint(self, a: Addr, v: Value) -> Context:
+        # Return FailedContext if v is None
+        pass
+
+    def astatus(self, a: Addr) -> AddrStatus:
+        pass
+
+    def all_pairs(self, e1: Expr, e2: Expr) -> \
+    Iterable[Tuple[Constant, Constant]]:
+        pass
+
+    def all_pairs(self, e1: Expr, e2: Expr, e3: Expr) -> \
+    Iterable[Tuple[Constant, Constant, Constant]]:
+        pass
+    
