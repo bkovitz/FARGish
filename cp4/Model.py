8ce929070e9662d3f03dee84ba9189215f1cabb3
Ben Kovitz <bkovitz@indiana.edu>
2023-03-02 11:02:58 -0500

    test_abc

--- cp4/Model.py
+++ cp4/Model.py
@@ -29,6 +29,54 @@ class Canvas:
     def make_from(cls, s: str) -> Canvas:
         return Canvas(s)
 
-    def __len__(self) -> int:
-        return 0
+    # TODO UT
+    def __getitem__(self, i: Index) -> str:
+        return self.contents[i - 1]
+        # TODO what if the index is out of bounds?
 
+    def __len__(self) -> int:
+        return len(self.contents)
+
+    def __str__(self) -> str:
+        return self.contents
+
+
+class Succ:
+
+    @classmethod
+    def has_relation(cls, x1: Any, x2: Any) -> bool:
+        match (x1, x2):
+            case (str(), str()):
+                if x1 >= 'z':
+                    return False
+                return ord(x1) + 1 == ord(x2)
+        return False
+
+    # TODO UT  Fail when reaching 'z'
+    @classmethod
+    def make(cls, start_letter: str, n: int) -> str:
+        result: List[str] = [start_letter]
+        for _ in range(n - 1):
+            result.append(cls.next_letter(result[-1]))
+        return ''.join(result)
+
+    # TODO UT  Fail when reaching 'z'
+    @classmethod
+    def next_letter(cls, letter: str) -> str:
+        return chr(ord(letter) + 1)
+
+
+def detect_repetition(canvas: Canvas) -> Optional[Tuple[Seed, Callable]]:
+    start_letter = canvas[1]
+    second_letter = canvas[2]
+    if Succ.has_relation(start_letter, second_letter):
+        # try Succ all the way through
+        perfect = Succ.make(start_letter, len(canvas))
+        if str(canvas) == perfect:
+            return Seed(start_letter, 1), Succ
+    return None
+    
+@dataclass(frozen=True)
+class Seed:
+    letter: str
+    i: Index
--- cp4/testModel.py
+++ cp4/testModel.py
@@ -3,7 +3,7 @@
 import unittest
 import inspect
 
-from Model import Canvas
+from Model import Canvas, detect_repetition, Seed, Succ
 
 from Log import lo, set_log_level
 from util import pts, reseed, short
@@ -11,6 +11,21 @@ from util import pts, reseed, short
 
 class TestCanvas(unittest.TestCase):
 
+    def test_str(self) -> None:
+        canvas = Canvas.make_from('abc')
+        self.assertEqual(str(canvas), 'abc')
+
     def test_len(self) -> None:
         canvas = Canvas.make_from('abc')
         self.assertEqual(len(canvas), 3)
+
+class TestRepetitionDetection(unittest.TestCase):
+
+    def test_abc(self) -> None:
+        #detector = D
+        canvas = Canvas.make_from('abc')
+        got = detect_repetition(canvas)
+        assert got is not None
+        seed, op = got
+        self.assertEqual(seed, Seed('a', 1))
+        self.assertEqual(op, Succ)
