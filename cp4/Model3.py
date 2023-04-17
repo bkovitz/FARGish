9eb0998cb36169585ca9da892c180796148d7029
Ben Kovitz <bkovitz@indiana.edu>
2023-04-17 06:57:15 -0400

    Added FakeIt for sequence of events

--- cp4/Model3.py
+++ cp4/Model3.py
@@ -8,23 +8,59 @@ from typing import Any, Callable, ClassVar, Collection, Dict, FrozenSet, \
     runtime_checkable, TYPE_CHECKING, no_type_check, get_type_hints, get_args
 from abc import ABC, abstractmethod
 
+from util import force_setattr, short
 
-def run(problem) -> str:
-    ws = problem_to_ws(problem)
-    
-    while True:
-        ws = process_A(ws)
-        ws = process_B(ws)
-        if time_to_stop(ws):
-            break
-    return ws.solution()
+
+def run(problem) -> Model:
+    m = Model()
+    m.run(problem)
+    m.events = [  #FAKE
+        Detected(Succ(at('C1', 1), at('C1', 2))),
+        MadePainter('P1', Succ(at('C1', 1), at('C1', 2))),
+        Detected(Succ(at('P1', 'left'), at('P1', 'right'))),
+        MadePainter('P2',
+            PainterCluster(
+                Define('PP1', Succ(At('AA1'), At('AA2'))),
+                Define('AA1', CanvasAddress('SS', 'II1')),
+                Define('AA2', CanvasAddress('SS', 'II2')),
+                Succ('II1', 'II2')
+            )
+        ),
+        RanPainter('P2', AA1=CanvasAddress('C1', 2)),
+        MadePainter('P3',
+            Succ(At(CanvasAddress('C1', 2)), At(CanvasAddress('C1', 3)))
+        ),
+        RanPainter('P3')
+    ]
+    return m
+
+@dataclass
+class Model:
+    ws: Workspace = field(default_factory=lambda: Workspace())
+    events: List[Event] = field(default_factory=lambda: [])
+
+    def run(self, problem) -> str:
+        self.ws = problem_to_ws(problem)
+        
+        while True:
+            self.ws = process_A(self.ws)
+            self.ws = process_B(self.ws)
+            if self.time_to_stop():
+                break
+        return self.ws.solution()
+
+    def time_to_stop(self) -> bool:
+        return self.ws.t >= 5
+
+    def solution(self) -> str:
+        return self.ws.solution()
 
 
 def process_A(ws: Workspace) -> Workspace:
     new_ws_objs = ws.run_detectors()
     return ws.add_objs(new_ws_objs)
 
-def process_B(ws) -> Workspace:
+def process_B(ws: Workspace) -> Workspace:
     p = choose_painter(ws)
     return p.run(ws)
 
@@ -35,22 +71,73 @@ def process_B(ws) -> Workspace:
 def problem_to_ws(s: str) -> Workspace:
     return Workspace().define_and_name(Canvas.from_str(s))
 
-def time_to_stop(ws: Workspace) -> bool:
-    return ws.t >= 5
-
 def choose_painter(ws: Workspace) -> Painter:
     return NullPainter()
 
-class Painter(ABC):
-    
-    @abstractmethod
-    def run(self, ws: Workspace) -> Workspace:
-        pass
+##### Variables and Parameters #####
 
-class NullPainter(Painter):
+T = TypeVar('T')
 
-    def run(self, ws: Workspace) -> Workspace:
-        return ws.bump_t()
+@dataclass(frozen=True)
+class Var:
+    '''A variable, together with a level number so that the model can tell
+    local variables in PainterClusters apart from variables of the same name in
+    an enclosing scope.'''
+    name: str
+    level: int
+
+    @classmethod
+    def at_level(cls, v: Union[str, Var, T], level: int) -> Union[Var, T]:
+        match v:
+            case str() if is_variable(v):
+                return Var(v, level)  # type: ignore[arg-type]
+            case Var():
+                return Var(v.name, level)
+#            case CompoundWorkspaceObj():
+#                return v.with_var_level(level)  # type: ignore[return-value]
+            case _:
+                return v  # type: ignore[return-value]
+        return v  # type: ignore[return-value]   # mypy bug
+
+    @classmethod
+    def is_at_least_level_1(cls, v: Argument) -> bool:
+        match v:
+            case Var(level=level):
+                return level >= 1
+            case _:
+                return False
+
+    @classmethod
+    def doubled_first_letter(cls, v: Variable, suffix: Optional[int]=None) \
+    -> Variable:
+        match v:
+            case str():
+                name = f'{v[0]}{v[0]}'
+                if suffix is None:
+                    return name
+                else:
+                    return name + str(suffix)
+            case Var(name, level):
+                new_name = name[0] + name[0]
+                if suffix is not None:
+                    new_name = new_name + str(suffix)
+                return Var(new_name, level)
+
+    @classmethod
+    def append_suffix(cls, var: Variable, suffix: str) -> Variable:
+        match var:
+            case str():
+                return var + suffix
+            case Var(name, level):
+                return Var(name + suffix, level)
+        
+Variable = Union[str, Var]
+Parameter = Union[Variable, T]
+OptionalParameter = Union[Variable, T, None]
+
+ParameterName = Literal['left', 'right']
+
+Index = int
 
 ##### CanvasObjects #####
 
@@ -94,7 +181,120 @@ class Canvas:
     @classmethod
     def from_str(cls, s: str):
         return Canvas(tuple(Letter.from_str(c) for c in s))
+
+##### Events #####
+
+@dataclass(frozen=True)
+class Event:
+    pass
+
+@dataclass(frozen=True)
+class Detected(Event):
+    what: Any
+
+@dataclass(frozen=True)
+class MadePainter(Event):
+    name: Variable
+    what: Any
+
+@dataclass(frozen=True)
+class RanPainter(Event):
+    name: Variable
+    args: Dict[str, Any]
+
+    def __init__(self, name: Variable, **kwargs):
+        force_setattr(self, 'name', name)
+        force_setattr(self, 'args', kwargs)
+
+##### Painters #####
+
+class Painter(ABC):
     
+    @abstractmethod
+    def run(self, ws: Workspace) -> Workspace:
+        pass
+
+class NullPainter(Painter):
+
+    def run(self, ws: Workspace) -> Workspace:
+        return ws.bump_t()
+
+@dataclass(frozen=True)
+class Succ(Painter):
+    left: Any
+    right: Any
+
+    def run(self, ws: Workspace) -> Workspace:
+        raise NotImplementedError
+
+@dataclass(frozen=True)
+class PainterCluster(Painter):
+    elems: Tuple[PainterClusterElem, ...]
+
+    def __init__(self, *elems: PainterClusterElem):
+        force_setattr(self, 'elems', elems)
+
+    def __eq__(self, other: Any) -> bool:
+        return (
+            isinstance(other, PainterCluster)
+            and
+            set(self.elems) == set(other.elems)
+        )
+
+    def run(self, ws: Workspace) -> Workspace:
+        raise NotImplementedError
+
+##### Objects that go inside Painters #####
+
+@dataclass(frozen=True)
+class At:
+    addr: Parameter[Address]
+
+def at(container: str, index: Any) -> At:
+    '''Shorthand for making an At, so you don't have to type CanvasAddress
+    or ParameterAddress.'''
+    return At(CanvasAddress(container, index))
+    
+@dataclass(frozen=True)
+class Define:
+    name: Variable
+    value: Any
+
+PainterClusterElem = Union[Define, Painter]
+
+##### Addresses #####
+
+@dataclass(frozen=True)
+class CanvasAddress:
+    canvas: Parameter[Canvas]
+    i: Parameter[Index]
+
+    def __str__(self) -> str:
+        cl = self.__class__.__name__
+        return f'{cl}({short(self.canvas)}, {self.i})'
+
+    __repr__ = __str__
+
+@dataclass(frozen=True)
+class ParameterAddress:
+    painter: Parameter[Painter]
+    parameter_name: Parameter[ParameterName]  # Can't be indirect, i.e.
+                                        # must be a variable, not a constant
+
+    def __repr__(self) -> str:
+        return f'PA({short(self.painter)}.{short(self.parameter_name)})'
+
+    __str__ = __repr__
+
+Address = Union[CanvasAddress, ParameterAddress]
+
+##### Types and TypeGuards #####
+
+def is_variable(x: Any) -> TypeGuard[Variable]:
+    return (isinstance(x, str) and len(x) > 1) or isinstance(x, Var)
+
+Argument = Any
+
 ##### The Workspace #####
 
 @dataclass(frozen=True)
--- cp4/atestModel3.py
+++ cp4/atestModel3.py
@@ -8,13 +8,72 @@ if 'unittest.util' in __import__('sys').modules:
     # Show full diff in self.assertEqual.
     __import__('sys').modules['unittest.util']._MAX_LENGTH = 999999999
 
-from Model3 import run
+from Model3 import at, run, At, Detected, Succ, MadePainter, PainterCluster, \
+    CanvasAddress, Define, RanPainter
 
 
 class TestModel3(unittest.TestCase):
 
     def test_ab_(self) -> None:
-        self.assertEqual(run('ab_'), 'abc')
+        m = run('ab_')
+        self.assertEqual(
+            m.events,
+            [ 
+                Detected(Succ(at('C1', 1), at('C1', 2))),
+                MadePainter('P1', Succ(at('C1', 1), at('C1', 2))),
+                Detected(Succ(at('P1', 'left'), at('P1', 'right'))),
+                MadePainter('P2',
+                    PainterCluster(
+                        Define('PP1', Succ(At('AA1'), At('AA2'))),
+                        Define('AA1', CanvasAddress('SS', 'II1')),
+                        Define('AA2', CanvasAddress('SS', 'II2')),
+                        Succ('II1', 'II2')
+                    )
+                ),
+                RanPainter('P2', AA1=CanvasAddress('C1', 2)),
+                MadePainter('P3',
+                    Succ(At(CanvasAddress('C1', 2)), At(CanvasAddress('C1', 3)))
+                ),
+                RanPainter('P3')
+            ]
+        )
+        """
+            [
+                #Detected(Succ(At(CanvasAddress('C1', 1)), At(CanvasAddress('C1', 2))))),
+                Detected(Succ(at('C1', 1), at('C1', 2))),
+                MadePainter(Succ(At('C1', 1), At('C1', 2)),
+                Detected(Succ(At('P1', 'left'), At('P1', 'right')),
+                MadePainter('P2',
+                    PainterCluster(
+                        Define('PP1', Succ(At('AA1'), At('AA2'))),
+                        Define('AA1', Address('SS', 'II1')),
+                        Define('AA2', Address('SS', 'II2')),
+                        Succ('II1', 'II2')
+                    )
+                )
+                RanPainter('P2', AA1=Address('C1', 2)),
+                MadePainter('P3',
+                    Succ(At(Address('C1', 2)), At(Address('C1', 3)))
+                ),
+                RanPainter('P3')
+
+#                Detected(SuccAt(Address('C1', 1), Address('C1', 2))),
+#                MadePainter(SuccAt(Address('C1', 1), Address('C1', 2)),
+#                Detected(SuccAt('P1', 'left'), Address('P1', 'right')),
+#                MadePainter('P2',
+#                    PainterCluster(
+#                        Define('PP1', SuccAt('AA1', 'AA2')),
+#                        Define('AA1', Address('SS', 'II1')),
+#                        Define('AA2', Address('SS', 'II2')),
+#                        Succ('II1', 'II2')
+#                    )
+#                )
+#                RanPainter('P2', AA1=Address('C1', 2)),
+#                MadePainter('P3', SuccAt(Address('C1', 2), Address('C1', 3))),
+#                RanPainter('P3')
+            ]
+        """
+        self.assertEqual(m.solution(), 'abc')
         # run Succ detector on 'ab_', get Succ((C1, 1), (C1, 2))
         # run ArgumentRelationDetector on that Succ, get PainterCluster
         # run PainterCluster on AA1=(C1, 2), get Succ((C1, 2), (C1, 3))
